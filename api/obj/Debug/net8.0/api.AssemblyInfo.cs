63f0cebc29917fc924e507e0ec79ec020fb47339
Artem Matok <artemmatok@gmail.com>
2024-08-13 10:34:19 +0300

    Next

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e514fe060a1512f15196e2c31fe373d95194f5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f911d699de99ea1026262cd8dfe8df6f23baa1a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
