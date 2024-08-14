a8b178bbc13f45bab1aff98b850dd7cd08483b24
Artem Matok <artemmatok@gmail.com>
2024-08-14 16:42:35 +0300

    Rewrite Front

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63f0cebc29917fc924e507e0ec79ec020fb47339")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56df81180f7a8452a7e0ba07fdcba14999036b91")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from './UiComponents/components/ui/button';
+
+
+function App() {
+  return (
+    <div className="mb">
+      <Button >Add</Button>
+    </div>
+  );
+}
+
+export default App;
